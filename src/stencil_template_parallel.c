b0514c37bbb1d034da489402c9bc9708ecdb23fa
zahra-ynp <zahra.ynp@gmail.com>
2025-06-29 18:54:33 +0200

    Enhance MPI communication error handling and memory management in stencil_template_parallel.c. Added error checks for MPI_Send and MPI_Recv calls, improved memory allocation logic, and ensured proper cleanup of allocated resources.

--- src/stencil_template_parallel.c
+++ src/stencil_template_parallel.c
@@ -148,29 +148,125 @@ int main(int argc, char **argv)
 
       if (my_rank % 2 == 0) { // EVEN RANKS: SEND FIRST
           // Send to neighbors only if they exist
-          if (neighbours[NORTH] != MPI_PROC_NULL) MPI_Send(&current_plane[1 * full_sizex + 1], sizex, MPI_DOUBLE, neighbours[NORTH], 0, myCOMM_WORLD);
-          if (neighbours[SOUTH] != MPI_PROC_NULL) MPI_Send(&current_plane[sizey * full_sizex + 1], sizex, MPI_DOUBLE, neighbours[SOUTH], 1, myCOMM_WORLD);
-          if (neighbours[EAST] != MPI_PROC_NULL) MPI_Send(buffers[SEND][EAST], sizey, MPI_DOUBLE, neighbours[EAST], 2, myCOMM_WORLD);
-          if (neighbours[WEST] != MPI_PROC_NULL) MPI_Send(buffers[SEND][WEST], sizey, MPI_DOUBLE, neighbours[WEST], 3, myCOMM_WORLD);
+          if (neighbours[NORTH] != MPI_PROC_NULL) {
+              int err = MPI_Send(&current_plane[1 * full_sizex + 1], sizex, MPI_DOUBLE, neighbours[NORTH], 0, myCOMM_WORLD);
+              if (err != MPI_SUCCESS) {
+                  fprintf(stderr, "ERROR: MPI_Send failed on rank %d to NORTH\n", my_rank);
+                  MPI_Abort(MPI_COMM_WORLD, 1);
+              }
+          }
+          if (neighbours[SOUTH] != MPI_PROC_NULL) {
+              int err = MPI_Send(&current_plane[sizey * full_sizex + 1], sizex, MPI_DOUBLE, neighbours[SOUTH], 1, myCOMM_WORLD);
+              if (err != MPI_SUCCESS) {
+                  fprintf(stderr, "ERROR: MPI_Send failed on rank %d to SOUTH\n", my_rank);
+                  MPI_Abort(MPI_COMM_WORLD, 1);
+              }
+          }
+          if (neighbours[EAST] != MPI_PROC_NULL) {
+              int err = MPI_Send(buffers[SEND][EAST], sizey, MPI_DOUBLE, neighbours[EAST], 2, myCOMM_WORLD);
+              if (err != MPI_SUCCESS) {
+                  fprintf(stderr, "ERROR: MPI_Send failed on rank %d to EAST\n", my_rank);
+                  MPI_Abort(MPI_COMM_WORLD, 1);
+              }
+          }
+          if (neighbours[WEST] != MPI_PROC_NULL) {
+              int err = MPI_Send(buffers[SEND][WEST], sizey, MPI_DOUBLE, neighbours[WEST], 3, myCOMM_WORLD);
+              if (err != MPI_SUCCESS) {
+                  fprintf(stderr, "ERROR: MPI_Send failed on rank %d to WEST\n", my_rank);
+                  MPI_Abort(MPI_COMM_WORLD, 1);
+              }
+          }
           
           // Receive from neighbors only if they exist
-          if (neighbours[SOUTH] != MPI_PROC_NULL) MPI_Recv(&current_plane[(sizey + 1) * full_sizex + 1], sizex, MPI_DOUBLE, neighbours[SOUTH], 1, myCOMM_WORLD, &status);
-          if (neighbours[NORTH] != MPI_PROC_NULL) MPI_Recv(&current_plane[0 * full_sizex + 1], sizex, MPI_DOUBLE, neighbours[NORTH], 0, myCOMM_WORLD, &status);
-          if (neighbours[WEST] != MPI_PROC_NULL) MPI_Recv(buffers[RECV][WEST], sizey, MPI_DOUBLE, neighbours[WEST], 3, myCOMM_WORLD, &status);
-          if (neighbours[EAST] != MPI_PROC_NULL) MPI_Recv(buffers[RECV][EAST], sizey, MPI_DOUBLE, neighbours[EAST], 2, myCOMM_WORLD, &status);
+          if (neighbours[SOUTH] != MPI_PROC_NULL) {
+              int err = MPI_Recv(&current_plane[(sizey + 1) * full_sizex + 1], sizex, MPI_DOUBLE, neighbours[SOUTH], 1, myCOMM_WORLD, &status);
+              if (err != MPI_SUCCESS) {
+                  fprintf(stderr, "ERROR: MPI_Recv failed on rank %d from SOUTH\n", my_rank);
+                  MPI_Abort(MPI_COMM_WORLD, 1);
+              }
+          }
+          if (neighbours[NORTH] != MPI_PROC_NULL) {
+              int err = MPI_Recv(&current_plane[0 * full_sizex + 1], sizex, MPI_DOUBLE, neighbours[NORTH], 0, myCOMM_WORLD, &status);
+              if (err != MPI_SUCCESS) {
+                  fprintf(stderr, "ERROR: MPI_Recv failed on rank %d from NORTH\n", my_rank);
+                  MPI_Abort(MPI_COMM_WORLD, 1);
+              }
+          }
+          if (neighbours[WEST] != MPI_PROC_NULL) {
+              int err = MPI_Recv(buffers[RECV][WEST], sizey, MPI_DOUBLE, neighbours[WEST], 3, myCOMM_WORLD, &status);
+              if (err != MPI_SUCCESS) {
+                  fprintf(stderr, "ERROR: MPI_Recv failed on rank %d from WEST\n", my_rank);
+                  MPI_Abort(MPI_COMM_WORLD, 1);
+              }
+          }
+          if (neighbours[EAST] != MPI_PROC_NULL) {
+              int err = MPI_Recv(buffers[RECV][EAST], sizey, MPI_DOUBLE, neighbours[EAST], 2, myCOMM_WORLD, &status);
+              if (err != MPI_SUCCESS) {
+                  fprintf(stderr, "ERROR: MPI_Recv failed on rank %d from EAST\n", my_rank);
+                  MPI_Abort(MPI_COMM_WORLD, 1);
+              }
+          }
 
       } else { // ODD RANKS: RECEIVE FIRST
           // Receive from neighbors only if they exist
-          if (neighbours[SOUTH] != MPI_PROC_NULL) MPI_Recv(&current_plane[(sizey + 1) * full_sizex + 1], sizex, MPI_DOUBLE, neighbours[SOUTH], 1, myCOMM_WORLD, &status);
-          if (neighbours[NORTH] != MPI_PROC_NULL) MPI_Recv(&current_plane[0 * full_sizex + 1], sizex, MPI_DOUBLE, neighbours[NORTH], 0, myCOMM_WORLD, &status);
-          if (neighbours[WEST] != MPI_PROC_NULL) MPI_Recv(buffers[RECV][WEST], sizey, MPI_DOUBLE, neighbours[WEST], 3, myCOMM_WORLD, &status);
-          if (neighbours[EAST] != MPI_PROC_NULL) MPI_Recv(buffers[RECV][EAST], sizey, MPI_DOUBLE, neighbours[EAST], 2, myCOMM_WORLD, &status);
+          if (neighbours[SOUTH] != MPI_PROC_NULL) {
+              int err = MPI_Recv(&current_plane[(sizey + 1) * full_sizex + 1], sizex, MPI_DOUBLE, neighbours[SOUTH], 1, myCOMM_WORLD, &status);
+              if (err != MPI_SUCCESS) {
+                  fprintf(stderr, "ERROR: MPI_Recv failed on rank %d from SOUTH\n", my_rank);
+                  MPI_Abort(MPI_COMM_WORLD, 1);
+              }
+          }
+          if (neighbours[NORTH] != MPI_PROC_NULL) {
+              int err = MPI_Recv(&current_plane[0 * full_sizex + 1], sizex, MPI_DOUBLE, neighbours[NORTH], 0, myCOMM_WORLD, &status);
+              if (err != MPI_SUCCESS) {
+                  fprintf(stderr, "ERROR: MPI_Recv failed on rank %d from NORTH\n", my_rank);
+                  MPI_Abort(MPI_COMM_WORLD, 1);
+              }
+          }
+          if (neighbours[WEST] != MPI_PROC_NULL) {
+              int err = MPI_Recv(buffers[RECV][WEST], sizey, MPI_DOUBLE, neighbours[WEST], 3, myCOMM_WORLD, &status);
+              if (err != MPI_SUCCESS) {
+                  fprintf(stderr, "ERROR: MPI_Recv failed on rank %d from WEST\n", my_rank);
+                  MPI_Abort(MPI_COMM_WORLD, 1);
+              }
+          }
+          if (neighbours[EAST] != MPI_PROC_NULL) {
+              int err = MPI_Recv(buffers[RECV][EAST], sizey, MPI_DOUBLE, neighbours[EAST], 2, myCOMM_WORLD, &status);
+              if (err != MPI_SUCCESS) {
+                  fprintf(stderr, "ERROR: MPI_Recv failed on rank %d from EAST\n", my_rank);
+                  MPI_Abort(MPI_COMM_WORLD, 1);
+              }
+          }
 
           // Send to neighbors only if they exist
-          if (neighbours[NORTH] != MPI_PROC_NULL) MPI_Send(&current_plane[1 * full_sizex + 1], sizex, MPI_DOUBLE, neighbours[NORTH], 0, myCOMM_WORLD);
-          if (neighbours[SOUTH] != MPI_PROC_NULL) MPI_Send(&current_plane[sizey * full_sizex + 1], sizex, MPI_DOUBLE, neighbours[SOUTH], 1, myCOMM_WORLD);
-          if (neighbours[EAST] != MPI_PROC_NULL) MPI_Send(buffers[SEND][EAST], sizey, MPI_DOUBLE, neighbours[EAST], 2, myCOMM_WORLD);
-          if (neighbours[WEST] != MPI_PROC_NULL) MPI_Send(buffers[SEND][WEST], sizey, MPI_DOUBLE, neighbours[WEST], 3, myCOMM_WORLD);
+          if (neighbours[NORTH] != MPI_PROC_NULL) {
+              int err = MPI_Send(&current_plane[1 * full_sizex + 1], sizex, MPI_DOUBLE, neighbours[NORTH], 0, myCOMM_WORLD);
+              if (err != MPI_SUCCESS) {
+                  fprintf(stderr, "ERROR: MPI_Send failed on rank %d to NORTH\n", my_rank);
+                  MPI_Abort(MPI_COMM_WORLD, 1);
+              }
+          }
+          if (neighbours[SOUTH] != MPI_PROC_NULL) {
+              int err = MPI_Send(&current_plane[sizey * full_sizex + 1], sizex, MPI_DOUBLE, neighbours[SOUTH], 1, myCOMM_WORLD);
+              if (err != MPI_SUCCESS) {
+                  fprintf(stderr, "ERROR: MPI_Send failed on rank %d to SOUTH\n", my_rank);
+                  MPI_Abort(MPI_COMM_WORLD, 1);
+              }
+          }
+          if (neighbours[EAST] != MPI_PROC_NULL) {
+              int err = MPI_Send(buffers[SEND][EAST], sizey, MPI_DOUBLE, neighbours[EAST], 2, myCOMM_WORLD);
+              if (err != MPI_SUCCESS) {
+                  fprintf(stderr, "ERROR: MPI_Send failed on rank %d to EAST\n", my_rank);
+                  MPI_Abort(MPI_COMM_WORLD, 1);
+              }
+          }
+          if (neighbours[WEST] != MPI_PROC_NULL) {
+              int err = MPI_Send(buffers[SEND][WEST], sizey, MPI_DOUBLE, neighbours[WEST], 3, myCOMM_WORLD);
+              if (err != MPI_SUCCESS) {
+                  fprintf(stderr, "ERROR: MPI_Send failed on rank %d to WEST\n", my_rank);
+                  MPI_Abort(MPI_COMM_WORLD, 1);
+              }
+          }
       }
 
       // [C] Unpack data from the pre-allocated RECV buffers
@@ -301,11 +397,233 @@ int initialize_sources( int       ,
 			vec2_t  ** );
 
 
-int memory_allocate ( const int       *,
-		      const vec2_t     ,
-		            buffers_t *,
-		            plane_t   * );
-		      
+int memory_allocate ( const int       *neighbours  ,
+		      const vec2_t     N           ,
+		            buffers_t *buffers_ptr ,
+		            plane_t   *planes_ptr
+		      )
+
+{
+    /*
+      here you allocate the memory buffers that you need to
+      (i)  hold the results of your computation
+      (ii) communicate with your neighbours
+
+      The memory layout that I propose to you is as follows:
+
+      (i) --- calculations
+      you need 2 memory regions: the "OLD" one that contains the
+      results for the step (i-1)th, and the "NEW" one that will contain
+      the updated results from the step ith.
+
+      Then, the "NEW" will be treated as "OLD" and viceversa.
+
+      These two memory regions are indexed by *plate_ptr:
+
+      planew_ptr[0] ==> the "OLD" region
+      plamew_ptr[1] ==> the "NEW" region
+
+
+      (ii) --- communications
+
+      you may need two buffers (one for sending and one for receiving)
+      for each one of your neighnours, that are at most 4:
+      north, south, east amd west.      
+
+      To them you need to communicate at most mysizex or mysizey
+      daouble data.
+
+      These buffers are indexed by the buffer_ptr pointer so
+      that
+
+      (*buffers_ptr)[SEND][ {NORTH,...,WEST} ] = .. some memory regions
+      (*buffers_ptr)[RECV][ {NORTH,...,WEST} ] = .. some memory regions
+      
+      --->> Of course you can change this layout as you prefer
+      
+     */
+
+  if (planes_ptr == NULL ){
+        // Print an error message to standard error.
+        fprintf(stderr, "ERROR in memory_allocate: received an invalid NULL pointer for planes.\n");
+        // Abort the entire MPI job, returning an error code of 1.
+        MPI_Abort(MPI_COMM_WORLD, 1);
+  }
+
+
+  if (buffers_ptr == NULL ){
+        // Print an error message to standard error.
+        fprintf(stderr, "ERROR in memory_allocate: received an invalid NULL pointer for buffers.\n");
+        // Abort the entire MPI job, returning an error code of 1.
+        MPI_Abort(MPI_COMM_WORLD, 1);
+   } 
+
+  // ··················································
+  // allocate memory for data
+
+  const int sizex = planes_ptr[OLD].size[_x_];
+  const int sizey = planes_ptr[OLD].size[_y_];
+  
+  // Check for valid domain sizes
+  if (sizex <= 0 || sizey <= 0) {
+      fprintf(stderr, "ERROR in memory_allocate: invalid domain size sizex=%d, sizey=%d\n", sizex, sizey);
+      MPI_Abort(MPI_COMM_WORLD, 1);
+  }
+  
+  // we allocate the space needed for the plane plus a contour frame
+  // that will contains data form neighbouring MPI tasks
+  unsigned int frame_size = (sizex + 2) * (sizey + 2);
+  
+  // Check for potential integer overflow
+  if (frame_size == 0 || frame_size / (sizex + 2) != (sizey + 2)) {
+      fprintf(stderr, "ERROR in memory_allocate: integer overflow in frame_size calculation\n");
+      MPI_Abort(MPI_COMM_WORLD, 1);
+  }
+
+  planes_ptr[OLD].data = (double*)malloc( frame_size * sizeof(double) );
+  if ( planes_ptr[OLD].data == NULL ){
+        fprintf(stderr, "ERROR in memory_allocate: failed to allocate memory for OLD plane (size: %u doubles)\n", frame_size);
+        MPI_Abort(MPI_COMM_WORLD, 1);
+  }
+  memset ( planes_ptr[OLD].data, 0, frame_size * sizeof(double) );
+
+  planes_ptr[NEW].data = (double*)malloc( frame_size * sizeof(double) );
+  if ( planes_ptr[NEW].data == NULL ){
+        fprintf(stderr, "ERROR in memory_allocate: failed to allocate memory for NEW plane (size: %u doubles)\n", frame_size);
+        free(planes_ptr[OLD].data); // Clean up the already allocated memory
+        MPI_Abort(MPI_COMM_WORLD, 1);
+  }
+  memset ( planes_ptr[NEW].data, 0, frame_size * sizeof(double) );
+
+  // ··················································
+  // buffers for north and south communication 
+  // are not really needed
+  //
+  // in fact, they are already contiguous, just the
+  // first and last line of every rank's plane
+  //
+  // you may just make some pointers pointing to the
+  // correct positions
+  //
+
+  // or, if you preer, just go on and allocate buffers
+  // also for north and south communications
+
+  // ··················································
+  // allocate buffers
+  if (neighbours[EAST] != MPI_PROC_NULL) {
+        buffers_ptr[SEND][EAST] = (double*)malloc(sizey * sizeof(double));
+        if(buffers_ptr[SEND][EAST] == NULL) {
+            fprintf(stderr, "ERROR in memory_allocate: failed to allocate EAST send buffer\n");
+            free(planes_ptr[OLD].data);
+            free(planes_ptr[NEW].data);
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+        buffers_ptr[RECV][EAST] = (double*)malloc(sizey * sizeof(double));
+        if(buffers_ptr[RECV][EAST] == NULL) {
+            fprintf(stderr, "ERROR in memory_allocate: failed to allocate EAST recv buffer\n");
+            free(planes_ptr[OLD].data);
+            free(planes_ptr[NEW].data);
+            free(buffers_ptr[SEND][EAST]);
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+  }
+  if (neighbours[WEST] != MPI_PROC_NULL) {
+        buffers_ptr[SEND][WEST] = (double*)malloc(sizey * sizeof(double));
+        if(buffers_ptr[SEND][WEST] == NULL) {
+            fprintf(stderr, "ERROR in memory_allocate: failed to allocate WEST send buffer\n");
+            free(planes_ptr[OLD].data);
+            free(planes_ptr[NEW].data);
+            if (neighbours[EAST] != MPI_PROC_NULL) {
+                free(buffers_ptr[SEND][EAST]);
+                free(buffers_ptr[RECV][EAST]);
+            }
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+        buffers_ptr[RECV][WEST] = (double*)malloc(sizey * sizeof(double));
+        if(buffers_ptr[RECV][WEST] == NULL) {
+            fprintf(stderr, "ERROR in memory_allocate: failed to allocate WEST recv buffer\n");
+            free(planes_ptr[OLD].data);
+            free(planes_ptr[NEW].data);
+            if (neighbours[EAST] != MPI_PROC_NULL) {
+                free(buffers_ptr[SEND][EAST]);
+                free(buffers_ptr[RECV][EAST]);
+            }
+            free(buffers_ptr[SEND][WEST]);
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+  }
+
+  // ··················································
+
+  
+  return 0;
+}
+
+
+
+int memory_release ( plane_t *planes, buffers_t *buffers_ptr )
+  
+{
+  if ( planes != NULL )
+    {
+      if ( planes[OLD].data != NULL )
+	free (planes[OLD].data);
+      
+      if ( planes[NEW].data != NULL )
+	free (planes[NEW].data);
+    }
+
+  // Safely free buffer memory - only free if pointers are not NULL
+  if (buffers_ptr != NULL) {
+      if (buffers_ptr[SEND][EAST] != NULL) {
+          free(buffers_ptr[SEND][EAST]);
+          buffers_ptr[SEND][EAST] = NULL;
+      }
+      if (buffers_ptr[RECV][EAST] != NULL) {
+          free(buffers_ptr[RECV][EAST]);
+          buffers_ptr[RECV][EAST] = NULL;
+      }
+      if (buffers_ptr[SEND][WEST] != NULL) {
+          free(buffers_ptr[SEND][WEST]);
+          buffers_ptr[SEND][WEST] = NULL;
+      }
+      if (buffers_ptr[RECV][WEST] != NULL) {
+          free(buffers_ptr[RECV][WEST]);
+          buffers_ptr[RECV][WEST] = NULL;
+      }
+  }
+      
+  return 0;
+}
+
+
+
+int output_energy_stat ( int step, plane_t *plane, double budget, int Me, MPI_Comm *Comm )
+{
+
+  double system_energy = 0;
+  double tot_system_energy = 0;
+  get_total_energy ( plane, &system_energy );
+  
+  MPI_Reduce ( &system_energy, &tot_system_energy, 1, MPI_DOUBLE, MPI_SUM, 0, *Comm );
+  
+  if ( Me == 0 )
+    {
+      if ( step >= 0 )
+	printf(" [ step %4d ] ", step ); fflush(stdout);
+
+      
+      printf( "total injected energy is %g, "
+	      "system energy is %g "
+	      "( in avg %g per grid point)\n",
+	      budget,
+	      tot_system_energy,
+	      tot_system_energy / (plane->size[_x_]*plane->size[_y_]) );
+    }
+  
+  return 0;
+}
 
 int initialize ( MPI_Comm *Comm,
 		 int      Me,                  // the rank of the calling process
@@ -343,11 +661,15 @@ int initialize ( MPI_Comm *Comm,
   *energy_per_source = 1.0;
 
   if ( planes == NULL ) {
-    // manage the situation
+    fprintf(stderr, "ERROR: planes pointer is NULL in initialize function\n");
+    return 1;
   }
 
-  planes[OLD].size[0] = planes[OLD].size[0] = 0;
-  planes[NEW].size[0] = planes[NEW].size[0] = 0;
+  // Initialize plane sizes to 0
+  planes[OLD].size[_x_] = 0;
+  planes[OLD].size[_y_] = 0;
+  planes[NEW].size[_x_] = 0;
+  planes[NEW].size[_y_] = 0;
   
   for ( int i = 0; i < 4; i++ )
     neighbours[i] = MPI_PROC_NULL;
@@ -522,10 +844,10 @@ int initialize ( MPI_Comm *Comm,
   r = (*S)[_y_] % Grid[_y_];
   mysize[_y_] = s + (Y < r);
 
-  planes[OLD].size[0] = mysize[0];
-  planes[OLD].size[1] = mysize[1];
-  planes[NEW].size[0] = mysize[0];
-  planes[NEW].size[1] = mysize[1];
+  planes[OLD].size[_x_] = mysize[_x_];
+  planes[OLD].size[_y_] = mysize[_y_];
+  planes[NEW].size[_x_] = mysize[_x_];
+  planes[NEW].size[_y_] = mysize[_y_];
   
 
   if ( verbose > 0 )
@@ -655,7 +977,6 @@ int initialize_sources( int       Me,
 
   return 0;
 }
-
 
 
 int memory_allocate ( const int       *neighbours  ,
@@ -833,3 +1154,5 @@ int output_energy_stat ( int step, plane_t *plane, double budget, int Me, MPI_Co
   
   return 0;
 }
+
+
