64332486c0accdf4e21eb90f64ba854f3b829a09
zahra-ynp <zahra.ynp@gmail.com>
2025-06-29 11:44:56 +0200

    update scheduling strategy in update_plane and get_total_energy functions to use guided scheduling for improved performance

--- include/stencil_template_parallel.h
+++ include/stencil_template_parallel.h
@@ -187,7 +187,7 @@ inline int update_plane ( const int      periodic,
         int thread_id = omp_get_thread_num();
         double t0 = omp_get_wtime();
         
-        #pragma omp for collapse(2) schedule(dynamic, 500)
+        #pragma omp for collapse(2) schedule(guided)
     	for (uint j = 1; j <= ysize; j++){
         	for ( uint i = 1; i <= xsize; i++)
        		{ 
@@ -270,7 +270,7 @@ inline int get_total_energy( plane_t *plane,
        int thread_id = omp_get_thread_num();
        double t0 = omp_get_wtime();
        
-       #pragma omp for collapse(2) reduction(+:totenergy) schedule(dynamic, 500)
+       #pragma omp for collapse(2) reduction(+:totenergy) schedule(guided)
         for ( int j = 1; j <= ysize; j++ ){
             for ( int i = 1; i <= xsize; i++ ){
                 totenergy += data[ IDX(i, j) ];
